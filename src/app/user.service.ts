52796a792ab68c72399640b55993d7c90fa3b6e2
EugeneZnm <eugenenzioki@gmail.com>
2018-08-17 18:01:19 +0300

    creation of promise

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -23,7 +23,18 @@ user: User;
       email: string;
       public_repos: number;
     }
-   
+    const promise = new Promise((resolve) => {
+      this.http.get<ApiResponse>('https://api.github.com/users/eugeneznm?access_token=aad4aef7cf6f937d79bc8dbef820c8eb33aae6ae')
+      .toPromise().then(response => {
+        this.user.name = response.name;
+        this.user.company = response.company;
+        this.user.bio = response.bio;
+        this.user.email = response.email;
+        console.log(response);
+        resolve();
+      });
+    });
+    return promise;
 
    }
 }
