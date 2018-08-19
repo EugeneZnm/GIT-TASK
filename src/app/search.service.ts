3faa683bc5173af805e1558f1968c54ee4a1134e
EugeneZnm <eugenenzioki@gmail.com>
2018-08-19 20:42:07 +0300

    editing user service

--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -19,8 +19,10 @@ export class SearchService {
       results: string;
     }
     const promise = new Promise ((resolve) =>
-      this.http.get<ApiResponse>('').toPromise().then(response => {
+      // tslint:disable-next-line:max-line-length
+      this.http.get<ApiResponse>('https://api.github.com/repos/eugeneznm?access_token=aad4aef7cf6f937d79bc8dbef820c8eb33aae6ae').toPromise().then(response => {
         this.search.results = response.results;
+        console.log(response);
         resolve();
       })
     );
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import {SearchService} from '../search.service';
+import { Search } from '../search';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
+  // inject service into providers
+  providers: [SearchService]
 })
 export class UserComponent implements OnInit {
+  search: Search;
 
-  constructor() { }
+  constructor(public http: HttpClient, public _search: SearchService) { }
 
   ngOnInit() {
+    this._search.searchReturn();
+    this.search = this._search.search;
   }
 
 }
