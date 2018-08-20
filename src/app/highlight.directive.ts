72a05377cd7f901ab101f9b2a0a5e73cadc9ba0b
EugeneZnm <eugenenzioki@gmail.com>
2018-08-20 12:35:47 +0300

    directive color

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -5,8 +5,8 @@ import { Directive, ElementRef } from '@angular/core';
 })
 export class HighlightDirective {
 
-  constructor(private elem: ElementRef) { 
-    this.elem.nativeElement.style.textDecoration = 'highlight';
+  constructor(private elem: ElementRef) {
+    this.elem.nativeElement.style.color = 'yellow';
   }
 
 }
