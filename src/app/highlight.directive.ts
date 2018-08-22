8aa3f4f5e53e48b5e2e466d3911b71d96254bb31
EugeneZnm <eugenenzioki@gmail.com>
2018-08-22 14:43:00 +0300

    background color changes

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -6,7 +6,7 @@ import { Directive, ElementRef } from '@angular/core';
 export class HighlightDirective {
 
   constructor(private elem: ElementRef) {
-    this.elem.nativeElement.style.color = 'yellow';
+    this.elem.nativeElement.style.color = 'green';
   }
 
 }
